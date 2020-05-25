5d387afbe3a86dafce7ad51c99869e2137e9a65f
AnnAnnaU <62073766+AnnAnnaU@users.noreply.github.com>
2020-05-25 22:40:44 +0300

    first commit

---
+++ star-db/src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-15dababsnbw"><div class="ldio-6x9157zizb">
+            <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+
+
+
+export default Spinner;
